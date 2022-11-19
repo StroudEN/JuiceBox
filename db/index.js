867f515337f0b0141c4488f5d6a57b6641a85ca3
StroudEN <preston.jhowell@gmail.com>
2022-11-18 19:03:36 -0600

    render changes

--- db/index.js
+++ db/index.js
@@ -1,5 +1,12 @@
 const pg = require('pg')
-const client = new pg.Client('postgres://localhost:5432/juicebox-dev')
+const client = new pg.Client({
+  connectionString: process.env.DB_URL||'postgres://localhost:5432/juicebox-dev',
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USERNAME || undefined,
+  password: process.env.DB_PASSWORD || undefined,
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || "juicebox-dev"
+})
 
 
 // ------------------------Get Functions--------------------------------------------------
